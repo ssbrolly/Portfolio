7c7d09b46d4679814967cd9360f136b8349a2440
Usong Cho <silvermouse4012@gmail.com>
2018-06-06 19:12:09 -0700

    added wow

---
+++ javascript/utils.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.middle_floating_menu ul.fbox_container li .box').off().click(function(){
+		var loc = $(this).attr('data-link');
+		window.location.href = loc;
+	});
+	
+	$('.middle_floating_menu ul.fbox_container li .box').hover(function(){
+		$(this).find('img').addClass('expand');
+	},function(){
+		$('.middle_floating_menu ul.fbox_container li .box img').removeClass('expand');
+	});
+});
